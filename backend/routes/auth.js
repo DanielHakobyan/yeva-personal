6d7955513e0610b097037e8cfd0800f6403c4d85
Daniel <danielhakobyan460@gmail.com>
2026-04-06 20:35:09 +0400

    bycript fixed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
 const User = require('../models/User');
